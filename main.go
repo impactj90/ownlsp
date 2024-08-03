1d68f4d81d96ff8a4194e875a58a4a6d193c94a3
Tolga Ayvazoglu <tolga.ayvazoglu@devoteam.com>
2024-08-03 09:38:50 +0200

    feat: add simple hacky logger, since we cannot print into stdout

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/impactj90/ownlsp/rpc"
 )
 
 func main() {
+    logger := getLogger("/Users/tolgaayvazoglu/projects/ownlsp/log.txt")
+    logger.Println("Hey, i started")
+
 	fmt.Println("hi")
 
 	scanner := bufio.NewScanner(os.Stdin)
@@ -21,3 +25,13 @@ func main() {
 }
 
 func handleMessage(_ any) {}
+
+func getLogger(filename string) *log.Logger {
+    logfile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+    if err != nil {
+        panic("hey, you didnt give me a goold file. I panic just for educational reasons.")
+    }
+
+    return log.New(logfile, "[odnLsp]", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
